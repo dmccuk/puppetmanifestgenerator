39d18d46a9cb2651300bc89b06c9c2db7cb60c3a
Dennis McCarthy <dmccuk@gmail.com>
2016-05-06 16:26:17 +0100

    First commit

---
+++ files.dm
@@ -0,0 +1,7 @@
+fstab /etc/fstab
+groups /etc/group
+resolv_conf /etc/resolv.conf
+sshd_config /etc/ssh/sshd_config
+sysctl /etc/sysctl.conf
+limits /etc/security/limits.conf
+hosts /etc/hosts
---
+++ manifest_creator.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Script to locally create a manifest using puppet resource
+# for local files.
+
+FILES=files.dm
+SERVICES=services_packages.dm
+HOST=`/bin/uname -n`
+
+# Create the directory framework.
+directory_framework()
+{
+while read NAME LOCATION; do
+  FS="/opt/$HOST/$NAME"
+  mkdir -p $FS/{manifests,templates}
+  cat $LOCATION > $FS/templates/$NAME".erb"
+  echo > $FS/manifests/init.pp
+  puppet resource file $LOCATION >> $FS/manifests/init.pp
+  sed -i -e '/mtime/d;/ctime/d;/md5/d;/type/d;/sel*/d' $FS/manifests/init.pp
+  sed -i "$ i\  content  => template(\"$NAME/templates/$NAME.erb\")" $FS/manifests/init.pp
+  sed -i 's/^/  /' $FS/manifests/init.pp
+  sed -i "1 i class $NAME {" $FS/manifests/init.pp
+  echo "}" >> $FS/manifests/init.pp
+done <$FILES
+}
+
+services_packages()
+{
+while read NAME PACKAGE; do
+  FS="/opt/$HOST/$NAME"
+  mkdir -p $FS/manifests
+  echo > $FS/manifests/init.pp
+  puppet resource package $PACKAGE >> $FS/manifests/init.pp
+  puppet resource service $NAME >> $FS/manifests/init.pp
+  sed -i 's/^/  /' $FS/manifests/init.pp
+  sed -i "1 i class $NAME {" $FS/manifests/init.pp
+  echo "}" >> $FS/manifests/init.pp
+done <$SERVICES
+}
+
+directory_framework
+services_packages
