4d3f35719ff62e3eb7112cc2fcb26201fb921dba
Dennis McCarthy <dmccuk@gmail.com>
2016-05-10 15:06:40 +0100

    catered for the hostname having a - and changed it to an _

--- manifest_creator.sh
+++ manifest_creator.sh
@@ -7,12 +7,21 @@ FILES=files_managed_by_templates.dm
 FILE_LINE=files_managed_by_file_line.dm
 SERVICES=services_packages.dm
 HOST=`/bin/uname -n`
+HOST_=`cat /tmp/hostname.dm`
+
+hostname_remove_any-()
+{
+# Remove any - from the servername and change them to _
+echo $HOST > /tmp/hostname.dm
+sed -i 's/-/_/g' /tmp/hostname.dm
+}
+
 
 # Create the directory framework.
 directory_framework()
 {
 while read NAME LOCATION; do
-  FS="/opt/$HOST/$NAME"
+  FS="/opt/$HOST_/$NAME"
   mkdir -p $FS/{manifests,templates}
   cat $LOCATION > $FS/templates/$NAME".erb"
   echo > $FS/manifests/init.pp
@@ -28,7 +37,7 @@ done <$FILES
 create_file_line_framework()
 {
 while read NAME1 LOCATION1; do
-  FS="/opt/$HOST/$NAME1"
+  FS="/opt/$HOST_/$NAME1"
   mkdir -p $FS/manifests
   echo > $FS/manifests/init.pp
   grep -vE '^(\s*$|#)' $LOCATION1| while read line
@@ -50,7 +59,7 @@ done <$FILE_LINE
 services_packages()
 {
 while read NAME PACKAGE; do
-  FS="/opt/$HOST/$NAME"
+  FS="/opt/$HOST_/$NAME"
   mkdir -p $FS/manifests
   echo > $FS/manifests/init.pp
   puppet resource package $PACKAGE >> $FS/manifests/init.pp
@@ -63,25 +72,25 @@ done <$SERVICES
 
 create_apply_file()
 {
-echo "#Execute this file to apply back the manifest locally" > /opt/$HOST/apply.pp
-for i in `ls /opt/$HOST`; do echo "puppet apply --modulepath=/opt/$HOST -e \"include $i\"" >> /opt/$HOST/apply.pp; done
-sed -i -e '/apply.pp/d' /opt/$HOST/apply.pp
+echo "#Execute this file to apply back the manifest locally" > /opt/$HOST_/apply.pp
+for i in `ls /opt/$HOST_`; do echo "puppet apply --modulepath=/opt/$HOST_ -e \"include $i\"" >> /opt/$HOST_/apply.pp; done
+sed -i -e '/apply.pp/d' /opt/$HOST_/apply.pp
 }
 
 create_role()
 {
-ROLE=/opt/$HOST/role_$HOST/manifest
+ROLE=/opt/$HOST_/role_$HOST_/manifest
 mkdir -p $ROLE
 echo "#Add this role to your puppet master. Either in hiera or the tool you use to manage your infrastructure" > $ROLE/init.pp
-for i in `ls /opt/$HOST`; do echo "  include $i" >> $ROLE/init.pp; done
-sed -i -e "/apply.pp/d;/role_$HOST/d" $ROLE/init.pp
-sed -i "2 i class role_$HOST { \n" $ROLE/init.pp
+for i in `ls /opt/$HOST_`; do echo "  include $i" >> $ROLE/init.pp; done
+sed -i -e "/apply.pp/d;/role_$HOST_/d" $ROLE/init.pp
+sed -i "2 i class role_$HOST_ { \n" $ROLE/init.pp
 echo "}" >> $ROLE/init.pp
 }
 
 replace_hostname_with_facter()
 {
-for i in `find /opt/$HOST/ -name *.erb`
+for i in `find /opt/$HOST_/ -name *.erb`
 do
 grep $HOST $i
   if [ $? == 0 ] ; then
@@ -91,6 +100,7 @@ done
 }
 
 
+hostname_remove_any-
 directory_framework
 create_file_line_framework
 services_packages
